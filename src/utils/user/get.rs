ca791ef737491d1bbcd073ee15566d6566aff540
NuggoDEV <NuggoDEV@gmail.com>
2023-09-07 18:07:25 +0000

    Split user into separate file

---
+++ src/utils/user/get.rs
@@ -0,0 +1,65 @@
+use actix_web::HttpResponse;
+use serde_json::to_string_pretty;
+use mongodb::bson::doc;
+use crate::service::mongo::USER_COLLECTION;
+
+pub async fn get_user(id: String) -> HttpResponse {
+    let collection = USER_COLLECTION.get().unwrap();
+    if let Ok(user) = collection.find_one(doc! { "game_id": id }, None).await {
+        if !user.is_none() {
+            let mut user = user.unwrap();
+            user.remove("_id");
+            user.remove("apiKey");
+            user.remove("sessionKey");
+            user.remove("sessionKeyExpires");
+            return HttpResponse::Ok()
+                .insert_header(("access-control-allow-origin", "*"))
+                .body(to_string_pretty(&user).unwrap().to_string());
+        }
+    }
+
+    HttpResponse::NotFound()
+        .insert_header(("access-control-allow-origin", "*"))
+        .body("User not found")
+}
+
+pub async fn get_username(id: String) -> String {
+    let collection = USER_COLLECTION.get().unwrap();
+    if let Ok(user) = collection.find_one(doc! { "game_id": id }, None).await {
+        if !user.is_none() {
+            return user.unwrap().get_str("username").unwrap().to_string();
+        }
+    }
+    "null".to_string()
+}
+
+pub async fn get_session_key(id: String) -> String {
+    let collection = USER_COLLECTION.get().unwrap();
+    if let Ok(user) = collection.find_one(doc! { "game_id": id }, None).await {
+        if !user.is_none() {
+            return user.unwrap().get_str("sessionKey").unwrap().to_string();
+        }
+    }
+    "null".to_string()
+}
+
+pub async fn get_api_key(id: String) -> String {
+    let collection = USER_COLLECTION.get().unwrap();
+    if let Ok(user) = collection.find_one(doc! { "game_id": id }, None).await {
+        if !user.is_none() {
+            return user.unwrap().get_str("apiKey").unwrap().to_string();
+        }
+    }
+    "null".to_string()
+}
+
+
+pub async fn get_session_key_time(id: String) -> i64 {
+    let collection = USER_COLLECTION.get().unwrap();
+    if let Ok(user) = collection.find_one(doc! { "game_id": id }, None).await {
+        if !user.is_none() {
+            return user.unwrap().get_i64("sessionKey").unwrap();
+        }
+    }
+    0
+}
---
+++ src/utils/user/post.rs
@@ -0,0 +1,77 @@
+use std::time::SystemTime;
+use mongodb::bson::doc;
+use actix_web::{web::Json, HttpResponse};
+use base64::{Engine as _, engine::general_purpose};
+use image::EncodableLayout;
+use serde_json::Value;
+use crate::service::models::*;
+
+use crate::service::mongo::USER_COLLECTION;
+
+pub async fn upload_avatar(avatar: String, id: String) {
+    let avatar = general_purpose::STANDARD.decode(avatar).unwrap();
+    let image = image::load_from_memory(avatar.as_bytes()).unwrap();
+    image.save_with_format(format!("./src/extras/Users/Avatars/{id}.png"), image::ImageFormat::Png).unwrap();
+}
+
+pub async fn login_user(body: Json<UserLoginModel>) -> HttpResponse {
+    let collection = USER_COLLECTION.get().unwrap();
+    if let Ok(user) = collection.find_one(doc! { "game_id": &body.user_id }, None).await {
+        if !user.is_none() {
+            let user = user.unwrap();
+            let username = user.get("username").unwrap();
+            let session_key = general_purpose::STANDARD.encode(generate_random_string(50));
+            let time_set: i64 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs().try_into().unwrap();
+            let update = doc! {
+                "$set": {
+                    "sessionKey": &session_key,
+                    "sessionKey": time_set + 21600
+                }
+            };
+            
+            collection.update_one(doc! { "game_id": &body.user_id }, update, None).await.unwrap();
+            println!("[API: /user/login] {} logged in. Session key: {}", username, &session_key);
+            return HttpResponse::Ok().body(doc! { "sessionKey": &session_key }.to_string());
+        }
+    }
+    HttpResponse::Ok().body("a")
+} 
+
+pub async fn create_user(body: Json<UserModel>) -> HttpResponse {
+    let collection = USER_COLLECTION.get().unwrap();
+    if let Ok(user) = collection.find_one(doc! { "game_id": &body.game_id}, None).await {
+        if user.is_none() {
+            let api_key = general_purpose::STANDARD.encode(generate_random_string(50));
+
+            let request: Value = reqwest::get(format!("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=E84E7BFB414CCF2EEAAF9FDCC337CAF7&steamids={}", &body.game_id)).await.unwrap().json().await.unwrap();
+            let url = request["response"]["players"][0]["avatarfull"].as_str().unwrap();
+            let avatar = reqwest::get(url).await.unwrap();
+
+            let image = image::load_from_memory(&avatar.bytes().await.unwrap().to_vec()).unwrap();
+            image.save_with_format(format!("./src/extras/Users/Avatars/{}.png", &body.game_id), image::ImageFormat::Png).unwrap();
+            
+            let new_user = doc! {
+                "discord_id": &body.discord_id,
+                "game_id": &body.game_id,
+                "username": &body.username,
+                "avatar": format!("https://api.thebedroom.party/user/{}/avatar", &body.game_id),
+                "apiKey": &api_key
+            };
+            collection.insert_one(new_user, None).await.unwrap();
+            println!("[API: /user/create] Created new user");
+            return HttpResponse::Ok().body(doc! { "apiKey": &api_key }.to_string());
+        }
+    }
+    HttpResponse::Conflict().body("User already exists")
+}
+
+fn generate_random_string(length: i32) -> String {
+    let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let mut rng = rand::thread_rng();
+    let random_string: String = (0..length)
+        .map(|_| {
+            let index = rand::Rng::gen_range(&mut rng, 0..characters.len());
+            characters.chars().nth(index).unwrap()
+        }).collect();
+    random_string
+}
