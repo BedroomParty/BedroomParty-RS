acedbe9241291ff7e59bfd079d64fa71698a91db
NuggoDEV <NuggoDEV@gmail.com>
2023-09-09 19:14:52 +0000

    Add web request to beatsaver for name + fix upload

--- src/utils/leaderboard/post.rs
+++ src/utils/leaderboard/post.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpResponse, web::Json};
 use mongodb::bson::doc;
+use serde_json::Value;
 use std::time::SystemTime;
 use crate::service::{mongo::LEADERBOARD_COLLECTION, models::ScoreModel};
 
@@ -22,7 +23,13 @@ pub async fn score_upload(hash: String, body: Json<ScoreModel>) -> HttpResponse 
         if leaderboard.is_none() {
             println!("[API: /leaderboard/{}/upload] Creating new leaderboard", &hash);
 
+            let request: Value = reqwest::get(format!("https://api.beatsaver.com/maps/hash/{}", &hash)).await.unwrap().json().await.unwrap();
+            if !request["error"].is_null() {
+                return HttpResponse::Forbidden().body("Leaderboard doesn't exist on BeatSaver, upload is forbidden");
+            }
+
             let new_leaderboard = doc! {
+                "name": request["name"].as_str().unwrap(),
                 "hash": &hash,
                 "scores": {
                     &body.characteristic: {
@@ -37,19 +44,18 @@ pub async fn score_upload(hash: String, body: Json<ScoreModel>) -> HttpResponse 
             return HttpResponse::Ok().body("Created new leaderboard and uploaded score.");
         }
         else {
-            println!("[API: leaderboard/{}/upload] Attempting upload...", &hash);
+            println!("[API: /leaderboard/{}/upload] Attempting upload...", &hash);
 
             let leaderboard = leaderboard.unwrap();
             let directory = format!("scores.{}.{}", &body.characteristic, &body.difficulty);
             let scores = leaderboard.get_document("scores").unwrap();
             if let Ok(characteristic) = scores.get_document(&body.characteristic) {
                 if let Ok(difficulty) = characteristic.get_array(&body.difficulty.to_string()) {
-
                     for score in difficulty {
                         let score = score.as_document().unwrap();
 
-                        if score.get("id").unwrap().to_string() == body.id {
-                            if score.get_i64("modifiedScore").unwrap() < body.modifiedScore {
+                        if score.get_str("id").unwrap() == body.id {
+                            if score.get_f64("accuracy").unwrap() < body.accuracy.try_into().unwrap() {
                                 let score_pull = doc! {
                                     "$pull": {
                                         &directory: {
@@ -57,17 +63,8 @@ pub async fn score_upload(hash: String, body: Json<ScoreModel>) -> HttpResponse 
                                         }
                                     }
                                 };
-
-                                let score_push = doc! {
-                                    "$push": {
-                                        &directory: &new_score
-                                    }
-                                };
+                                
                                 collection.update_one(doc! { "hash": &hash }, score_pull, None).await.unwrap(); // Deletes current score
-                                collection.update_one(doc! { "hash": &hash }, score_push, None).await.unwrap(); // Uploads new score
-
-                                println!("[API: /leaderboard/{}/upload] Successfully uploaded", &hash);
-                                return HttpResponse::Ok().body("Score uploaded");
                             }
                             else  {
                                 return HttpResponse::Conflict().body("Not a highscore");
@@ -75,6 +72,14 @@ pub async fn score_upload(hash: String, body: Json<ScoreModel>) -> HttpResponse 
                         }
                     }
                 }
+                let score_push = doc! {
+                    "$push": {
+                        &directory: &new_score
+                    }
+                };
+                collection.update_one(doc! { "hash": &hash }, score_push, None).await.unwrap();
+                println!("[API: /leaderboard/{}/upload] Successfully uploaded", &hash);
+                return HttpResponse::Ok().body("Score uploaded");
             }
         }
     }
